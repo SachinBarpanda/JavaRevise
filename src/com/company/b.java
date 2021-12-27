7b8e4bccdfbde9464482afe2b6340e116e837f4a
SachinBarpanda <barpandasachin>
2021-12-27 23:57:12 +0530

    2nd question

---
+++ src/com/company/b.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        int vol = a*b*c;
+        int area = ((a*b)+(b*c)+(c*a));
+        System.out.println("Volume : " + vol+" Area : "+area);
+    }
+}
