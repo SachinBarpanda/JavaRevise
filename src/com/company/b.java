d8bb3c305ea3d9f12933fbec02a7ab6d4490e4bc
SachinBarpanda <sachinongithub@gmail.com>
2021-12-28 16:00:00 +0530

    check 2

--- src/com/company/b.java
+++ src/com/company/b.java
@@ -14,6 +14,6 @@ public class b {
         int vol = a*b*c;
         int area = ((a*b)+(b*c)+(c*a));
         System.out.println("Volume : " + vol+" Area : "+area);
-        System.out.println();
+        System.out.println("Area of one side : "+ a*b);
     }
 }
