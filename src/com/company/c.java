59948cdbe4a10e63c76e0441cf80416d794559d3
SachinBarpanda <sachinongithub@gmail.com>
2022-01-01 23:54:11 +0530

    shift

---
+++ src/com/company/c.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class c {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a<<1);//left shift *2
+        System.out.println(a>>1);
+    }
+}
